609b1a4a1d46a41fbd8e3803c44b24e18269ca9f
Brevven <brevingame@gmail.com>
2023-01-03 00:18:46 -0800

    some settings

--- me.lua
+++ me.lua
@@ -1,30 +1,16 @@
 local me = {}
 
-me.name = "bzchlorine"
+me.name = "bzgold"
 me.list = {}
 
-function me.starting_patch() 
-  return (
-      me.get_setting("bzchlorine-early-salt") == "mining" or
-      me.get_setting("bzchlorine-early-salt") == "both"
-  )
-end
 
-function me.early_assemblers() 
-  return (
-      me.get_setting("bzchlorine-early-salt") == "assembler" or
-      me.get_setting("bzchlorine-early-salt") == "both" or
-      (not me.salt())
-  )
-end
-
-function me.more() 
+function me.silver() 
   if me.get_setting("bz-all-intermediates") then return true end
-  return me.get_setting("bzchlorine-more-intermediates") == "yes"
+  return me.get_setting("bzgold-silver")
 end
 
-function me.salt() 
-  return me.get_setting("bzchlorine-salt")
+function me.catalysis() 
+  return me.get_setting("bzgold-catalysis")
 end
 
 function me.get_setting(name)
