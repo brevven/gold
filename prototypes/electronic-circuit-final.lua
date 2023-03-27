da7458d209f88570d0080ee7ebe39782c15ea9fe
Brevven <brevingame@gmail.com>
2023-03-27 01:22:23 -0700

    aai electronics recipes

--- prototypes/electronic-circuit-final.lua
+++ prototypes/electronic-circuit-final.lua
@@ -40,5 +40,36 @@ if util.me.silver() then
     else
       util.add_unlock("silver-processing", "electronic-circuit-silver")
     end
+
+    if mods["aai-industry"] and not mods.bzgas then
+      util.set_main_product("electronic-circuit-stone", "electronic-circuit")
+      local ec2 = futil.table.deepcopy(data.raw.recipe["electronic-circuit-stone"])
+      ec2.name = "electronic-circuit-stone-silver"
+      data:extend({ec2})
+      util.set_enabled("electronic-circuit-stone-silver", false)
+      util.replace_ingredient("electronic-circuit-stone-silver", "copper-cable", "silver-wire")
+      util.add_icon("electronic-circuit-stone-silver", {
+        icon = "__bzgold__/graphics/icons/silver-wire.png",
+        icon_size = 64, icon_mipmaps = 4, scale = 0.3, shift = {8,-8}
+      })
+      util.add_icon("electronic-circuit-stone-silver", {
+        icon = "__aai-industry__/graphics/icons/stone-tablet.png",
+        icon_size = 64, scale = 0.3, shift = {-8,-8}
+      })
+      util.add_icon("electronic-circuit", {
+        icon = "__base__/graphics/icons/copper-cable.png",
+        icon_size = 64, icon_mipmaps = 4, scale = 0.3, shift = {8,-8}
+      })
+      util.add_icon("electronic-circuit-stone", {
+        icon = "__aai-industry__/graphics/icons/stone-tablet.png",
+        icon_size = 64, scale = 0.3, shift = {-8,-8}
+      })
+      if util.check_unlock("electronics", "electronic-circuit-stone") then
+        util.add_unlock("electronics", "electronic-circuit-stone-silver")
+      else
+        util.add_unlock("silver-processing", "electronic-circuit-stone-silver")
+      end
+    end
+  end
 end
 
